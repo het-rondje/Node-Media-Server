d3c30c9583c5fc76072fcf894608fdbc34874348
Ricky Van den Berg <ricky.vandenberg@hotmail.nl>
2019-06-13 20:50:44 +0200

    addd library and some requests

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const NodeMediaServer = require('./');
+const axios = require('axios');
+
+const apiConfig = {
+  url: '',
+
+}
 
 const config = {
   rtmp: {
@@ -48,29 +54,47 @@ nms.on('doneConnect', (id, args) => {
 
 nms.on('prePublish', (id, StreamPath, args) => {
   console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  console.log("==============pre");
+
+
+  axios.post(`/users/${StreamPath}`, {})
+  .then((res) => {
+    //if the response is not ok reject the session
+  });
   // let session = nms.getSession(id);
   // session.reject();
 });
 
 nms.on('postPublish', (id, StreamPath, args) => {
   console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  console.log("==============post");
+
+  axios.post(`/users/${StreamPath}`, { online: true })
+  .then((res) => {
+    
+  })
 });
 
 nms.on('donePublish', (id, StreamPath, args) => {
   console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+
+  console.log("==============done");
 });
 
 nms.on('prePlay', (id, StreamPath, args) => {
   console.log('[NodeEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  console.log("==============preplay");
   // let session = nms.getSession(id);
   // session.reject();
 });
 
 nms.on('postPlay', (id, StreamPath, args) => {
   console.log('[NodeEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  console.log("==============postplay");
 });
 
 nms.on('donePlay', (id, StreamPath, args) => {
   console.log('[NodeEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+  console.log("==============doneplay");
 });
 
