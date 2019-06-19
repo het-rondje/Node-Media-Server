1e1f5e440735ff22388b9b44f690a886e8658107
Ricky van den berg <ricky.vandenberg@hotmail.nl>
2019-06-19 11:03:18 +0200

    remove auth

--- app.js
+++ app.js
@@ -58,11 +58,7 @@ nms.on('prePublish', (id, StreamPath, args) => {
   console.log('[NodeEvent on prePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
   console.log("==============pre");
 
-
-  instance.post(`users/cUy--uvu-`, { randomString: "dfc6r2vq29dtuboiadn" }, { headers: {
-    signature: "8f33e1116a1e47aabba23287098a0020883ea874979c9c4ba71e99499470833e2dbba3a62318f93420ed642c13f79c3402841de669b5e8a38109064b9334c8cfef3bb9142546ba004a6a3c98035f5058448a7b22bf6547631b4865976e42c9b26db283678f4d69980d85e7006f9ef33d28148a9f61a9a43a7363633b378ef724f141266b6bdb0aa4792bcb1683a0c6aab834644b094adbd9c99ddd122f90a8458ab1ddfe0f4730348d092c61ff7e5cb97b523e33aa4ec03588c917d0a245aea49ffc7dc4459e294e9dc88731f00ffb4cd7c12d9400bd9fd503be4e3c2840de6ede0d8877572382807a263aee14183acecdcaa54ec8ded733f0951518bbb9043e",
-    userid: "cUy--uvu-"
-  }})
+  instance.post(`users/cUy--uvu-`)
   .then((res) => {
     //TODO maybe logging?
   }).catch((e) => {
@@ -75,10 +71,7 @@ nms.on('postPublish', (id, StreamPath, args) => {
   console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
   console.log("==============post");
 
-  instance.post(`streams/cUy--uvu-`, { online: true }, { headers: {
-    signature: "8f33e1116a1e47aabba23287098a0020883ea874979c9c4ba71e99499470833e2dbba3a62318f93420ed642c13f79c3402841de669b5e8a38109064b9334c8cfef3bb9142546ba004a6a3c98035f5058448a7b22bf6547631b4865976e42c9b26db283678f4d69980d85e7006f9ef33d28148a9f61a9a43a7363633b378ef724f141266b6bdb0aa4792bcb1683a0c6aab834644b094adbd9c99ddd122f90a8458ab1ddfe0f4730348d092c61ff7e5cb97b523e33aa4ec03588c917d0a245aea49ffc7dc4459e294e9dc88731f00ffb4cd7c12d9400bd9fd503be4e3c2840de6ede0d8877572382807a263aee14183acecdcaa54ec8ded733f0951518bbb9043e",
-    userid: "cUy--uvu-"
-  }})
+  instance.post(`streams/cUy--uvu-`, { online: true })
   .then((res) => {
     //TODO logging or handling response
   }).catch((e) => {
@@ -87,10 +80,7 @@ nms.on('postPublish', (id, StreamPath, args) => {
 });
 
 nms.on('donePublish', (id, StreamPath, args) => {
-  instance.post(`streams/cUy--uvu-`, { online: false }, { headers: {
-    signature: "8f33e1116a1e47aabba23287098a0020883ea874979c9c4ba71e99499470833e2dbba3a62318f93420ed642c13f79c3402841de669b5e8a38109064b9334c8cfef3bb9142546ba004a6a3c98035f5058448a7b22bf6547631b4865976e42c9b26db283678f4d69980d85e7006f9ef33d28148a9f61a9a43a7363633b378ef724f141266b6bdb0aa4792bcb1683a0c6aab834644b094adbd9c99ddd122f90a8458ab1ddfe0f4730348d092c61ff7e5cb97b523e33aa4ec03588c917d0a245aea49ffc7dc4459e294e9dc88731f00ffb4cd7c12d9400bd9fd503be4e3c2840de6ede0d8877572382807a263aee14183acecdcaa54ec8ded733f0951518bbb9043e",
-    userid: "cUy--uvu-"
-  }})
+  instance.post(`streams/cUy--uvu-`, { online: false })
   .then((res) => {
     //TODO logging or handling response
   }).catch((e) => {
